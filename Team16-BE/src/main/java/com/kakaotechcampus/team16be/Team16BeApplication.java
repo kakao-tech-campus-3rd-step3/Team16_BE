b15ceb3afe47ddee623ef80f441a6eabf4e6a10b
LeeJaeRyun <pinixs3@naver.com>
2025-08-23 15:58:53 +0900

    chore: 스프링 JDK-21 초기 개발 환경 설정

---
+++ Team16-BE/src/main/java/com/kakaotechcampus/team16be/Team16BeApplication.java
@@ -0,0 +1,13 @@
+package com.kakaotechcampus.team16be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team16BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team16BeApplication.class, args);
+    }
+
+}
---
+++ Team16-BE/src/test/java/com/kakaotechcampus/team16be/Team16BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaotechcampus.team16be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team16BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
