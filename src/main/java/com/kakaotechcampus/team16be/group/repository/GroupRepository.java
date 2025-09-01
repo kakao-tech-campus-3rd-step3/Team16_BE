58c9b4b955d752facf46adf0b74e3c366ef82557
xodbs126 <dltvmsskd@naver.com>
2025-09-01 22:49:28 +0900

    feat: 애너테이션 추가

--- src/main/java/com/kakaotechcampus/team16be/group/repository/GroupRepository.java
+++ src/main/java/com/kakaotechcampus/team16be/group/repository/GroupRepository.java
@@ -2,6 +2,8 @@ package com.kakaotechcampus.team16be.group.repository;
 
 import com.kakaotechcampus.team16be.group.domain.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GroupRepository extends JpaRepository<Group,Long> {
 }
--- src/main/java/com/kakaotechcampus/team16be/group/service/GroupServiceImpl.java
+++ src/main/java/com/kakaotechcampus/team16be/group/service/GroupServiceImpl.java
@@ -1,4 +1,7 @@
 package com.kakaotechcampus.team16be.group.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GroupServiceImpl implements GroupService{
 }
