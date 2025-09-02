5cfeeee7216f22e405b16a81afe07b5c2ab81aa8
LeeJaeRyun <pinixs3@naver.com>
2025-09-02 14:25:36 +0900

    Refactor: 다대다 관계를 위한 User 엔티티 리팩토링

--- src/main/java/com/kakaotechcampus/team16be/user/domain/User.java
+++ src/main/java/com/kakaotechcampus/team16be/user/domain/User.java
@@ -6,34 +6,32 @@ import lombok.Getter;
 
 @Entity
 @Getter
+@Table(name = "users")
 public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "kakaoId", length = 64)
+    @Column(name = "kakao_id", unique = true, nullable = false, length = 64)
     private String kakaoId;
 
-    @Column(name = "nickname", length = 40)
+    @Column(name = "nickname", unique = true, nullable = false, length = 40)
     private String nickname;
 
-    @Column(name = "profileImageUrl", length = 512)
+    @Column(name = "profile_image_url", length = 512)
     private String profileImageUrl;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "role")
+    @Column(name = "role", nullable = false)
     private Role role;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "verificationStatus")
+    @Column(name = "verification_status", nullable = false)
     private VerificationStatus verificationStatus;
 
-    @Column(name = "studentIdImageUrl", length = 512)
+    @Column(name = "student_id_image_url", length = 512)
     private String studentIdImageUrl;
-
-    @Column(name = "groupId")
-    private Long groupId;
 
     @Column(name = "score")
     private Long score;
