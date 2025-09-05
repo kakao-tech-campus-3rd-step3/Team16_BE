8b4b4871025e25b2932bd4d767691c480936c749
LeeJaeRyun <pinixs3@naver.com>
2025-09-05 12:50:42 +0900

    fix: @EnableJpaAuditing 중복 문제 수정

--- src/main/java/com/kakaotechcampus/team16be/Team16BeApplication.java
+++ src/main/java/com/kakaotechcampus/team16be/Team16BeApplication.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableJpaAuditing
 public class Team16BeApplication {
 
     public static void main(String[] args) {
