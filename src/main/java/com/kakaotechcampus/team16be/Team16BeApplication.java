388862090559506183748fc67e41ee72af4f75f0
LeeJaeRyun <pinixs3@naver.com>
2025-09-07 15:53:37 +0900

    refactor: .env 환경변수를 Spring Boot에서 자동 로딩하도록 설정

--- src/main/java/com/kakaotechcampus/team16be/Team16BeApplication.java
+++ src/main/java/com/kakaotechcampus/team16be/Team16BeApplication.java
@@ -1,6 +1,5 @@
 package com.kakaotechcampus.team16be;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,19 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Team16BeApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure()
-                .directory("./")  // 최상위 폴더에 .env 위치
-                .load();
-
-        System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
-        System.setProperty("KAKAO_CLIENT_ID", dotenv.get("KAKAO_CLIENT_ID"));
-        System.setProperty("KAKAO_REDIRECT_URI", dotenv.get("KAKAO_REDIRECT_URI"));
-
-        System.setProperty("AWS_ACCESS_KEY_ID", dotenv.get("AWS_ACCESS_KEY_ID"));
-        System.setProperty("AWS_SECRET_ACCESS_KEY", dotenv.get("AWS_SECRET_ACCESS_KEY"));
-        System.setProperty("AWS_REGION", dotenv.get("AWS_REGION"));
-        System.setProperty("AWS_S3_BUCKET", dotenv.get("AWS_S3_BUCKET"));
-
         SpringApplication.run(Team16BeApplication.class, args);
     }
 
