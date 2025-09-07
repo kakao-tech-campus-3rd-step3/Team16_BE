db2e0c5a643d0fa0b073aa65dfd3ccf312fe3960
LeeJaeRyun <pinixs3@naver.com>
2025-09-07 16:14:43 +0900

    fix: presigned URL 업로드 시 userId를 JWT 기반 인증으로 변경 (@LoginUser 적용)

--- src/main/java/com/kakaotechcampus/team16be/aws/controller/ImageController.java
+++ src/main/java/com/kakaotechcampus/team16be/aws/controller/ImageController.java
@@ -3,6 +3,7 @@ package com.kakaotechcampus.team16be.aws.controller;
 import com.kakaotechcampus.team16be.aws.dto.ImageUrlDto;
 import com.kakaotechcampus.team16be.aws.dto.IssuePresignedUrlRequest;
 import com.kakaotechcampus.team16be.aws.service.S3UploadPresignedUrlService;
+import com.kakaotechcampus.team16be.common.annotation.LoginUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,9 +19,12 @@ public class ImageController {
     private final S3UploadPresignedUrlService s3UploadPresignedUrlService;
 
     @PostMapping("/presigned")
-    public ResponseEntity<ImageUrlDto> createPresignedUrl(@RequestBody IssuePresignedUrlRequest request) {
+    public ResponseEntity<ImageUrlDto> createPresignedUrl(
+            @LoginUser Long userId,
+            @RequestBody IssuePresignedUrlRequest request
+    ) {
         ImageUrlDto imageUrlDto = s3UploadPresignedUrlService.execute(
-                request.userId(), request.fileExtension(), request.type());
+                userId, request.fileExtension(), request.type());
         return ResponseEntity.ok(imageUrlDto);
     }
 
--- src/main/java/com/kakaotechcampus/team16be/aws/dto/IssuePresignedUrlRequest.java
+++ src/main/java/com/kakaotechcampus/team16be/aws/dto/IssuePresignedUrlRequest.java
@@ -4,7 +4,6 @@ import com.kakaotechcampus.team16be.aws.domain.ImageFileExtension;
 import com.kakaotechcampus.team16be.aws.domain.ImageUploadType;
 
 public record IssuePresignedUrlRequest(
-        Long userId, //누가 업로드 하는지 식별용 (user의 기본키인 userId)
         ImageFileExtension fileExtension, //확장자
         ImageUploadType type //업로드 타입
 ) {}
