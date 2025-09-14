b3a4856211e0baca2007573071898e7c47eb43cd
LeeJaeRyun <pinixs3@naver.com>
2025-09-15 02:56:04 +0900

    fix: VerificationStatus의 enum값과 주석의 enum값이 일치하지 않는 부분 수정

--- src/main/java/com/kakaotechcampus/team16be/auth/dto/StudentVerificationStatusResponse.java
+++ src/main/java/com/kakaotechcampus/team16be/auth/dto/StudentVerificationStatusResponse.java
@@ -3,9 +3,9 @@ package com.kakaotechcampus.team16be.auth.dto;
 import com.kakaotechcampus.team16be.user.domain.VerificationStatus;
 
 public record StudentVerificationStatusResponse (
-        boolean valid,//유저가 DB에 존재하는지 여부
-        VerificationStatus verificationStatus,//인증 상태 ("PENDING", "APPROVED", "REJECTED")
-        String reason//거절 사유
+        boolean valid,
+        VerificationStatus verificationStatus,
+        String reason
 ){
     public static StudentVerificationStatusResponse of(
             boolean valid,
