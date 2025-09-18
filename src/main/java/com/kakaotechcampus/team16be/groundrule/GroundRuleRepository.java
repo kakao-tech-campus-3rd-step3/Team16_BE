40d0db27e724f3f2375ce2a81f3be1b0c01da253
sungchan <awdr1057@naver.com>
2025-09-18 16:41:06 +0900

    refactor: 충돌을 인한 임시 수정(pull 후 그라운드룰 브랜치에서 수정)
    # Conflicts:
    #	src/main/java/com/kakaotechcampus/team16be/groundrule/GroundRuleController.java
    #	src/main/java/com/kakaotechcampus/team16be/groundrule/service/GroundRuleService.java
    #	src/main/java/com/kakaotechcampus/team16be/groundrule/service/GroundRuleServiceImpl.java

--- src/main/java/com/kakaotechcampus/team16be/groundrule/GroundRuleRepository.java
+++ src/main/java/com/kakaotechcampus/team16be/groundrule/GroundRuleRepository.java
@@ -1,5 +1,6 @@
 package com.kakaotechcampus.team16be.groundrule;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface GroundRuleRepository extends JpaRepository<GroundRule, Long>{
   Optional<GroundRule> findByGroupId(Long groupId);
+  List<GroundRule> findAllByGroupId(Long groupId);
 }
