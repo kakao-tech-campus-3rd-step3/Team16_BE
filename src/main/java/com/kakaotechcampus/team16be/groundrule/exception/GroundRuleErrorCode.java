302d4fac9b91db4d03a2ee3ae041a69e5f512821
sungchan <awdr1057@naver.com>
2025-09-21 15:33:11 +0900

    feat: RULE-002 추가

--- src/main/java/com/kakaotechcampus/team16be/groundrule/exception/GroundRuleErrorCode.java
+++ src/main/java/com/kakaotechcampus/team16be/groundrule/exception/GroundRuleErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum GroundRuleErrorCode {
 
-  RULE_NOT_FOUND(HttpStatus.NOT_FOUND, "RULE-001", "해당 룰이 존재하지 않습니다.");
+  RULE_NOT_FOUND(HttpStatus.NOT_FOUND, "RULE-001", "해당 룰이 존재하지 않습니다."),
+  RULE_NOT_ADD(HttpStatus.BAD_REQUEST, "RULE-002", "모임 당 룰의 최대 개수는 5개입니다.");
 
   private final HttpStatus status;
   private final String code;
