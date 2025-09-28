ab2b262a823f546fd454e9e3f6f49c0de1403ab8
LeeJaeRyun <pinixs3@naver.com>
2025-09-29 01:09:56 +0900

    feat: 일정 참여자 컨트롤러에 스웨거 적용

--- src/main/java/com/kakaotechcampus/team16be/planParticipant/PlanParticipantController.java
+++ src/main/java/com/kakaotechcampus/team16be/planParticipant/PlanParticipantController.java
@@ -6,6 +6,9 @@ import com.kakaotechcampus.team16be.planParticipant.service.PlanParticipantServi
 import com.kakaotechcampus.team16be.user.domain.User;
 import java.net.URI;
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/plans/{planId}/participants")
+@Tag(name = "일정 참여자", description = "일정 참여 관련 API")
 public class PlanParticipantController {
 
   private final PlanParticipantService planParticipantService;
+
+  @Operation(summary = "일정 참여", description = "로그인한 유저가 특정 일정에 참여합니다.")
   @PostMapping
   public ResponseEntity<Void> attendPlan(
       @LoginUser User user,
@@ -31,11 +37,13 @@ public class PlanParticipantController {
     return ResponseEntity.created(location).build();
   }
 
+  @Operation(summary = "일정 참여자 조회", description = "특정 일정에 참여한 모든 유저를 조회합니다.")
   @GetMapping
   public ResponseEntity<List<PlanParticipantResponseDto>> getAllParticipants(@PathVariable Long planId){
     return ResponseEntity.ok(planParticipantService.getAllParticipants(planId));
   }
 
+  @Operation(summary = "일정 참여 취소", description = "로그인한 유저가 특정 일정에 대한 참여를 취소합니다.")
   @PatchMapping
   public ResponseEntity<Void> withdrawAttendance(
       @LoginUser User user,
