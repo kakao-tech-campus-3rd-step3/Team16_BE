8f08def37ebf11d634d72af9c4d842a9314f2ad7
LeeJaeRyun <pinixs3@naver.com>
2025-10-01 02:00:16 +0900

    feat: 전역 예외 처리에 Unexpected Exception 핸들링 추가 및 로깅 설정

--- src/main/java/com/kakaotechcampus/team16be/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kakaotechcampus/team16be/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.kakaotechcampus.team16be.common.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -12,5 +15,17 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(e.getStatus())
                 .body(new ErrorResponseDto(e.getMessage(), e.getCode(), e.getStatus()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleException(Exception e) {
+        log.error("예상치 못한 서버 오류 발생!!", e);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponseDto(
+                        "예상치 못한 서버 오류가 발생했습니다.",
+                        "UNEXPECTED_ERROR",
+                        HttpStatus.INTERNAL_SERVER_ERROR.value()
+                ));
     }
 }
