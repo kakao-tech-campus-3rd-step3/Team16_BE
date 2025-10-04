65b53d51061a64bd5f4f53a1cb774fcb135f041b
xodbs126 <dltvmsskd@naver.com>
2025-10-05 01:00:48 +0900

    feat: 그룹별 GroupMember반환

---
+++ src/main/java/com/kakaotechcampus/team16be/groupMember/dto/GroupMemberDto.java
@@ -0,0 +1,25 @@
+package com.kakaotechcampus.team16be.groupMember.dto;
+
+import com.kakaotechcampus.team16be.groupMember.domain.GroupMember;
+import com.kakaotechcampus.team16be.groupMember.domain.GroupRole;
+
+import java.util.List;
+
+public record GroupMemberDto(
+        Long id,
+        String groupName,
+        String nickname,
+        GroupRole groupRole
+) {
+
+    public static List<GroupMemberDto> from(List<GroupMember> members) {
+        return members.stream()
+                .map(member -> new GroupMemberDto(
+                        member.getId(),
+                        member.getGroup().getName(),
+                        member.getUser().getNickname(),
+                        member.getRole()
+                ))
+                .toList();
+    }
+}
