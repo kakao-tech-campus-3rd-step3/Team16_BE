cd30f560d0d30bbc4b68a947f68b2e159478827a
LeeJaeRyun <pinixs3@naver.com>
2025-10-07 18:09:20 +0900

    feat: 로그인 유저 그룹 히스토리 DTO 추가

---
+++ src/main/java/com/kakaotechcampus/team16be/user/dto/UserGroupHistoryResponse.java
@@ -0,0 +1,30 @@
+package com.kakaotechcampus.team16be.user.dto;
+
+import com.kakaotechcampus.team16be.group.domain.SafetyTag;
+import com.kakaotechcampus.team16be.groupMember.domain.GroupMember;
+import com.kakaotechcampus.team16be.groupMember.domain.GroupMemberStatus;
+
+import java.time.format.DateTimeFormatter;
+
+public record UserGroupHistoryResponse(
+        String name,
+        GroupMemberStatus groupMemberStatus,
+        SafetyTag safetyTag,
+        String joinAt,
+        String leftAt
+) {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy. MM. dd");
+
+    public static UserGroupHistoryResponse from(GroupMember member) {
+        String joinAt = member.getCreatedAt() != null ? member.getCreatedAt().format(FORMATTER) : null;
+        String leftAt = member.getLeftAt() != null ? member.getLeftAt().format(FORMATTER) : null;
+
+        return new UserGroupHistoryResponse(
+                member.getGroup().getName(),
+                member.getStatus(),
+                member.getGroup().getSafetyTag(),
+                joinAt,
+                leftAt
+        );
+    }
+}
