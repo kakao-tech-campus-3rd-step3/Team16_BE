c0b7d1b8a8c49fe81e04ad02900c146292bf96a5
LeeJaeRyun <pinixs3@naver.com>
2025-10-07 19:55:29 +0900

    fix: /api/users/me 학생 인증 상태 enum으로 반환하도록 변경

--- src/main/java/com/kakaotechcampus/team16be/user/dto/UserInfoResponse.java
+++ src/main/java/com/kakaotechcampus/team16be/user/dto/UserInfoResponse.java
@@ -1,6 +1,7 @@
 package com.kakaotechcampus.team16be.user.dto;
 
 import com.kakaotechcampus.team16be.user.domain.User;
+import com.kakaotechcampus.team16be.user.domain.VerificationStatus;
 
 import java.util.List;
 import java.util.Map;
@@ -9,7 +10,7 @@ public record UserInfoResponse(
         String id,
         String nickname,
         String profileImageUrl,
-        boolean isStudentVerified,
+        VerificationStatus isStudentVerified,
         Map<String, List<String>> groups
 ) {
     public static UserInfoResponse of(User user, Map<String, List<String>> groups) {
@@ -17,7 +18,7 @@ public record UserInfoResponse(
                 user.getId().toString(),
                 user.getNickname(),
                 user.getProfileImageUrl(),
-                user.isStudentVerified(),
+                user.getVerificationStatus(),
                 groups
         );
     }
