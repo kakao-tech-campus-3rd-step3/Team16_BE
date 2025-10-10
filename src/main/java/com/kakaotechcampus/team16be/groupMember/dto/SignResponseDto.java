cd307594787c4f371c921428290521a2d260b43d
xodbs126 <dltvmsskd@naver.com>
2025-10-10 12:00:55 +0900

    fix:이미ìUser § 공개 URL 반환

--- src/main/java/com/kakaotechcampus/team16be/groupMember/dto/SignResponseDto.java
+++ src/main/java/com/kakaotechcampus/team16be/groupMember/dto/SignResponseDto.java
@@ -1,18 +1,19 @@
 package com.kakaotechcampus.team16be.groupMember.dto;
 
 import com.kakaotechcampus.team16be.groupMember.domain.GroupMember;
-
 import java.util.List;
 
 public record SignResponseDto(
         Long userId,
-        String intro
+        String intro,
+        String profileImageUrl
 ) {
     public static List<SignResponseDto> from(List<GroupMember> members) {
         return members.stream()
                 .map(m -> new SignResponseDto(
                         m.getUser().getId(),
-                        m.getIntro()
+                        m.getIntro(),
+                        m.getUser().getProfileImageUrl()
                 ))
                 .toList();
     }
