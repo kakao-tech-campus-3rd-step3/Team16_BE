63db8dfa6d5e04b551cbf74ace677d2cadccb59d
LeeJaeRyun <pinixs3@naver.com>
2025-11-05 13:26:19 +0900

    chore: 로드 밸런서 Health Check용으로 사용

---
+++ src/main/java/com/kakaotechcampus/team16be/admin/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.kakaotechcampus.team16be.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HealthCheckController {
+
+    @GetMapping("/healthcheck")
+    public String healthcheck() {
+        return "OK";
+    }
+
+}
